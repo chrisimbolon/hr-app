fbbc828dc3885e061186a086fc4d1cfdd23b01a2
Christyan Simbolon <sambungdata@gmail.com>
2026-04-03 00:42:11 +0700

    feat-backend: fix auth model

--- backend/app/modules/auth/infrastructure/models.py
+++ backend/app/modules/auth/infrastructure/models.py
@@ -1,3 +1,6 @@
+
+# app/modules/auth/infrastructure/models.py
+import uuid
 from datetime import datetime
 from uuid import UUID
 
@@ -5,18 +8,26 @@ from sqlmodel import Field, SQLModel
 
 
 class UserModel(SQLModel, table=True):
-    id: UUID = Field(primary_key=True)
-    email: str = Field(index=True)
-    is_active: bool
-    token_version: int
-    created_at: datetime
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    email: str = Field(index=True, unique=True)
+    is_active: bool = Field(default=True)
+    token_version: int = Field(default=0)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
 
 
 class CredentialModel(SQLModel, table=True):
-    user_id: UUID = Field(foreign_key="usermodel.id")
+    user_id: uuid.UUID = Field(
+        foreign_key="usermodel.id",
+        primary_key=True
+    )
     password_hash: str
 
-
 class UserTenantModel(SQLModel, table=True):
-    user_id: UUID = Field(foreign_key="usermodel.id")
-    tenant_id: UUID = Field(index=True)
+    user_id: uuid.UUID = Field(
+        foreign_key="usermodel.id",
+        primary_key=True
+    )
+    tenant_id: uuid.UUID = Field(
+        primary_key=True,
+        index=True
+    )
