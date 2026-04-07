30be5184d343bff004319eacfdc87a7fd7b431b2
Christyan Simbolon <sambungdata@gmail.com>
2026-04-08 00:58:50 +0700

    chore: setup next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/v1/:path*`,
+      },
+    ]
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.r2.cloudflarestorage.com' },
+      { protocol: 'https', hostname: '**.hadir.id' },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
